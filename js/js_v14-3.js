c11e641d9f077c7027650bdb28f71b11e4591e80
kawillrich <66137285+kawillrich@users.noreply.github.com>
2025-02-08 12:06:39 -0500

    feat: set container height width to viewport height width

--- js/js_v14-3.js
+++ js/js_v14-3.js
@@ -249,11 +249,9 @@ window.addEventListener(
 
 window.addEventListener("resize", function() {
   window.scrollTo(0, 0);
-
-  if (this.window.screen.orientation.type === "landscape" || this.window.screen.orientation.type === "landscape-primary") {
-    // console.log('landscape')
-  }
-  // this.document.getElementById("viewport").setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no")
+  let containerElement = document.querySelector("#container");
+  containerElement.style.width = windowViewportWidth;
+  containerElement.style.height = windowViewportHeight;
 })
 
 let images;
