4fccee94fcc8fc875f3bf41596afdbd14ca76f8c
kawillrich <66137285+kawillrich@users.noreply.github.com>
2025-02-19 15:36:28 -0500

    feat: move #character-info to top and adjust grid for mobile

--- js/js_v14-3.js
+++ js/js_v14-3.js
@@ -248,7 +248,7 @@ window.addEventListener(
     window.scrollTo(0, 0);
     let containerElement = document.querySelector("#container");    
     containerElement.style.width = visualViewportWidth - 60;
-    containerElement.style.height = visualViewportHeight;
+    containerElement.style.height = visualViewportHeight - 60;
     console.log (containerElement.style.width); 
     console.log ("container width: " + containerElement.offsetWidth); 
 
@@ -1088,11 +1088,13 @@ function updateAlignmentDescription() {
 
 //TEST FUNCTION TO START GAME - creates player info
 function startGame() {
-  $("#character-info").animate({width: "100%", height: "50%"}, 300).slideDown(300);
+  // $("#character-info").animate({width: "100%", height: "50%"}, 300).slideDown(300);
+  $("#character-info").slideDown(300);
 
 
 
-  // $("#character-info").css({margin: "0px"}).animate({width: "0px", height: "0px"}, 300).slideDown(300).next().dequeue();
+
+  $("#character-info").css({margin: "0px"}).animate({width: "0px", height: "0px"}, 300).slideDown(300).next().dequeue();
   window.scrollTo(0, 0);
   let strengthPlusAdjustment;
   let intelligencePlusAdjustment;
