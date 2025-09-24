24aec554869e7ba747ca537b59961cc72861a2fc
kawillrich <66137285+kawillrich@users.noreply.github.com>
2025-09-23 21:20:35 -0400

    feat: populated utility functions: createButton, createDialogue, continueAnimation

---
+++ js/Utilities/utilityFunctions.js
@@ -0,0 +1,47 @@
+function createDialogue(dialogueToCreate) {
+  dialogue.textContent = "";
+  let dialogueDiv = document.createElement("div");
+  dialogueDiv.id = `dialogue-div-${continueButtonSequence}`;
+  dialogueDiv.textContent = dialogueInput + " " + continueButtonSequence;
+  dialogue.appendChild(dialogueDiv);
+  dialogueDiv.appendChild(createButton(dialogueInput));
+};
+
+function createContinueButton(eventToAdd) {
+  let newButton = document.createElement("button");
+  newButton.id = `button-${buttonIterator}`;
+  newButton.type = "button";
+  newButton.textContent = `Continue ${buttonIterator}`;
+  newButton.style.display = "block";
+  newButton.addEventListener(
+    "click",
+    function () {
+      createDialogueDiv(dialogueToDisplay);
+    },
+    false
+  );
+  let lineBreak = document.createElement('br');
+  let appDiv = document.getElementById("dialogue");
+  appDiv.appendChild(lineBreak);
+  appDiv.appendChild(newButton);
+  buttonIterator++;
+  continueButtonSequence++;
+  return newButton; 
+};
+
+function continueAnimation() {
+  let newText = nextChapterText;
+  if (dialogueIterator < newText.length) {
+    dialogue.textContent += newText.charAt(dialogueIterator);
+    dialogueIterator++;
+    setTimeoutArray.push(setTimeout(function() {continueAnimation(newText)}, typingSpeed));
+  } else {
+    console.log('test')
+    for (let i = 0; i < setTimeoutArray.length; i ++) {
+      clearTimeout[i];
+    }
+    setTimeoutArray = [];
+    // addIntroContinueButton();
+    createButton(nextDialogue);
+  }  
+}
