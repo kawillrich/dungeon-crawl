0d48b589d19b05855cb11304cae78c52de21cf8a
kawillrich <66137285+kawillrich@users.noreply.github.com>
2025-10-05 12:14:34 -0400

    feat: remove roll and confirm attributes buttons when confirming

--- js/js_v14-3.js
+++ js/js_v14-3.js
@@ -895,7 +895,6 @@ function generateNewAttributes() {
 }
 
 function confirmingAttributes() {
-  let confirmStart = document.querySelector('#confirm-start');
   window.scrollTo(0, 0);
   let rollNewAttributes = document.querySelector("#generate-attributes");
 
@@ -905,11 +904,13 @@ function confirmingAttributes() {
   rollNewAttributes.style.cursor = "not-allowed";
   rollNewAttributes.style.disabled = true;
   rollNewAttributes.style.opacity = 0.5;
+  rollNewAttributes.remove();
 
   confirmAttributes.style.pointerEvents = "none";
   confirmAttributes.style.cursor = "not-allowed";
   confirmAttributes.style.disabled = true;
   confirmAttributes.style.opacity = 0.5;
+  confirmAttributes.remove();
 
   finalCharacter.attributes = attributes;
 
