9d1a39575a94572304d4fd1e2b9b84cea8bed67f
kawillrich <66137285+kawillrich@users.noreply.github.com>
2025-10-26 16:44:55 -0400

    style: adjust display to none on ul.hide-spell-list-dropdown when hidden

--- js/js_v14-3.js
+++ js/js_v14-3.js
@@ -3384,8 +3384,8 @@ function addingFightModule(monsterOne, monsterTwo, continueNextChapter) {
   dialogueDiv.appendChild(fightModuleContainer);
   headerFightModule.innerHTML = `
     <div class="attack-buttons">
-        <span class='button-border'><input type="submit" class="attack attack-monster-one fight-module-button" value="Attack Enemy 1"></span>
-        <span class='button-border'><input type="submit" class="attack attack-monster-two fight-module-button" value="Attack Enemy 2"></span>
+        <span class='button-border'><input type="submit" class="attack attack-monster-one fight-module-button" value="Attack"></span>
+        <span class='button-border'><input type="submit" class="attack attack-monster-two fight-module-button" value="Attack2"></span>
         <span class='button-border'><input type="submit" class="attack item-enchantment fight-module-button" value="Use Item"></span> 
         <span class='button-border'>
           <input type="submit" class="attack-spell spell2-monster-one fight-module-button" value="Use Spell" id="use-spells">
